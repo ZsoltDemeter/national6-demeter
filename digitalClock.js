29ad67372a0703a7dabc084d1dbc26a7cf9673d0
ZsoltDemeter <zsoltd.com@gmail.com>
2021-03-02 18:37:31 +0200

    not finished yet: 1,2,3 done only and no styling

--- digitalClock.js
+++ digitalClock.js
@@ -8,29 +8,40 @@ let secondsParagraph = document.querySelectorAll(".seconds p");
 let minutesParagraph = document.querySelectorAll(".minutes p");
 let hoursParagraph = document.querySelectorAll(".hours p");
 
-setInterval(function(){
+let varTimer;
 
-    renderDigits(seconds,secondsParagraph);
-    renderDigits(minutes,minutesParagraph);
-    renderDigits(hours,hoursParagraph);
+document.getElementById("start").addEventListener("click",function(){
+    varTimer = setInterval(myTimer,500);
+})
 
-    seconds++;
-    
-    if(seconds === 60){
-        seconds = 0;
-        minutes++;
-    }
+document.getElementById("stop").addEventListener("click",function(){
+    clearInterval(varTimer);
+})
 
-    if(minutes === 60){
-        minutes = 0;
-        hours++;
-    }
+document.getElementById("reset").addEventListener("click",function(){
+    clearInterval(varTimer);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
 
-    if(hours === 24){
-        hours = 0;
-    }
+    secondsParagraph[0].innerText = 0;
+    secondsParagraph[1].innerText = 0;
 
-},500);
+    minutesParagraph[0].innerText = 0;
+    minutesParagraph[1].innerText = 0;
+
+    hoursParagraph[0].innerText = 0;
+    hoursParagraph[1].innerText = 0;
+})
+
+document.getElementById("save").addEventListener("click",function(){
+    clearInterval(varTimer);
+
+    const savedTime = document.createElement("p");
+    document.querySelector(".saved-times").appendChild(savedTime);
+
+})
+
 
 function renderDigits(nr,pList){
     const numberString = nr + "";
@@ -44,6 +55,29 @@ function renderDigits(nr,pList){
         pList[1].innerText = numberStringArray[0];
     }
 }
+
+function myTimer(){
+        renderDigits(seconds,secondsParagraph);
+        renderDigits(minutes,minutesParagraph);
+        renderDigits(hours,hoursParagraph);
+    
+        seconds++;
+        
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+        }
+    
+        if(minutes === 60){
+            minutes = 0;
+            hours++;
+        }
+    
+        if(hours === 24){
+            hours = 0;
+        }
+}
+
 
 
 
