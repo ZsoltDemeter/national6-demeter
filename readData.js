88819db99d7046f11569ee042ab602b008be21c6
ZsoltDemeter <zsoltd.com@gmail.com>
2021-03-11 19:31:42 +0200

    testing

--- readData.js
+++ readData.js
@@ -31,27 +31,41 @@ function renderArticle(articleData){
     const articleTitle = document.createElement("h3");
     const articleContent = document.createElement("p");
 
+    articleListHtml.appendChild(article);
     article.appendChild(articleTitle);
     article.appendChild(articleContent);
 
-    articleListHtml.appendChild(article);
 
     articleTitle.innerText = articleData.title;
     articleContent.innerText = articleData.content;
 
-    fetch(`https://simple-json-server-scit.herokuapp.com/comments?postId=${articleData.postId}`);
+    fetch(`https://simple-json-server-scit.herokuapp.com/comments?postId=<${articleData.postId}>`).then(handleFetchResponse).then(useJSONResponse2);
+
+    function useJSONResponse2(index){
+        renderComments(index);
+    }
     
-    const commentsList = document.createElement("div");
-    const comments = document.createElement("div");
-    const user = document.createElement("h4");
-    const userComment = document.createElement("p");
+    function renderComments(commentList){
+        for(const comment of commentList){
+            renderComment(comment);
+        }
+    }
 
-    article.appendChild(commentsList);
-    commentsList.appendChild(comments);
-    comments.appendChild(user);
-    comments.appendChild(userComment);
+    function renderComment(commentData){
+        const commentsList = document.createElement("div");
+        const comments = document.createElement("div");
+        const user = document.createElement("h4");
+        const userComment = document.createElement("p");
 
-    user.innerText = articleData.username;
-    userComment.innerText = articleData.content;
+        
+        article.appendChild(commentsList);
+        commentsList.appendChild(comments);
+        comments.appendChild(user);
+        comments.appendChild(userComment);
+    
+        user.innerText = commentData.username;
+        userComment.innerText = commentData.content;
           
+    }
+       
 }
