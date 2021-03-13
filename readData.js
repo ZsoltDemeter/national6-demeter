af8197b78d16c6eed51235461381780e1a09c3fb
ZsoltDemeter <zsoltd.com@gmail.com>
2021-03-13 18:17:24 +0200

    finished homework

--- readData.js
+++ readData.js
@@ -5,15 +5,11 @@ console.log("Javascript - AJAX");
 document.getElementById("get-data").addEventListener("click",getData);
 
 function getData(){
-    fetch("https://simple-json-server-scit.herokuapp.com/posts").then(handleFetchResponse).then(useJSONResponse);
+    fetch("https://simple-json-server-scit.herokuapp.com/posts").then(handleFetchResponse).then(renderArticles);
 }
 
 function handleFetchResponse(response){
     return response.json();
-}
-
-function useJSONResponse(index){
-    renderArticles(index);
 }
 
 function renderArticles(articleList){
@@ -23,49 +19,49 @@ function renderArticles(articleList){
         renderArticle(articleData);
     }
 }
-
+const allComments = document.getElementsByClassName("comment-list");
 const articleListHtml = document.querySelector(".article-list");
 
 function renderArticle(articleData){
     const article = document.createElement("div");
     const articleTitle = document.createElement("h3");
     const articleContent = document.createElement("p");
+    const commentList = document.createElement("div");
+    commentList.classList.add("comment-list");
 
     articleListHtml.appendChild(article);
+
     article.appendChild(articleTitle);
     article.appendChild(articleContent);
+    article.appendChild(commentList);
 
 
     articleTitle.innerText = articleData.title;
     articleContent.innerText = articleData.content;
 
-    fetch(`https://simple-json-server-scit.herokuapp.com/comments?postId=<${articleData.postId}>`).then(handleFetchResponse).then(useJSONResponse2);
+    fetch(`https://simple-json-server-scit.herokuapp.com/comments?postId=${articleData.id}`).then(handleFetchResponse).then(renderComments);
+}
 
-    function useJSONResponse2(index){
-        renderComments(index);
+function renderComments(commentData){
+    for(const comment of commentData){
+        renderComment(comment);
+        console.log(commentData);
     }
-    
-    function renderComments(commentList){
-        for(const comment of commentList){
-            renderComment(comment);
-        }
-    }
+}
 
-    function renderComment(commentData){
-        const commentsList = document.createElement("div");
-        const comments = document.createElement("div");
-        const user = document.createElement("h4");
-        const userComment = document.createElement("p");
+function renderComment(commentData){
+    const comments = document.createElement("div");
+    comments.classList.add("comment");
+    const user = document.createElement("h4");
+    user.classList.add("comment-user");
+    const userComment = document.createElement("p");
+    userComment.classList.add("comment-content");
 
-        
-        article.appendChild(commentsList);
-        commentsList.appendChild(comments);
-        comments.appendChild(user);
-        comments.appendChild(userComment);
-    
-        user.innerText = commentData.username;
-        userComment.innerText = commentData.content;
-          
-    }
-       
+    allComments[commentData.postId].appendChild(comments);
+    comments.appendChild(user);
+    comments.appendChild(userComment);
+
+    user.innerText = commentData.username;
+    userComment.innerText = commentData.content;
+      
 }
