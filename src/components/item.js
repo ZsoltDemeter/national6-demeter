350caeeee2357f81b97339c5ac91ac0dfb4a4053
ZsoltDemeter <zsoltd.com@gmail.com>
2021-04-13 21:12:48 +0300

    finished course

---
+++ src/components/item.js
@@ -0,0 +1,58 @@
+import { updateUserData, getToDoData } from "../utils/api";
+
+const itemListContainer = document.getElementById("item-list");
+
+function createToDoItem(name, checked, todoList){
+    const container = document.createElement("div");
+    container.classList.add("todo-item");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = checked;
+    const text = document.createElement("p");
+    text.innerText = name;
+    const remove = document.createElement("img");
+    remove.src = "delete.png";
+    remove.classList.add("remove-button");
+
+
+    checkbox.addEventListener("click", () => {
+        const newList = todoList.map((element) => {
+            if(element.item === name){
+                element.checked = checkbox.checked;
+            }
+            return element;
+        });
+        updateUserData(newList, () =>{
+            getToDoData((data) => createToDoItemList(data.todo));
+        });
+    });
+
+    remove.addEventListener("click", () => {
+        const filteredList = todoList.filter((element) =>{
+            return element.item !== name;
+        });
+
+        updateUserData(filteredList, () =>{
+            getToDoData((data) => createToDoItemList(data.todo));
+        });
+    });
+
+    container.appendChild(checkbox);
+    container.appendChild(text);
+    container.appendChild(remove);
+
+    return container;
+}
+
+export function createToDoItemList(todoList) {
+    itemListContainer.innerHTML = "";
+  
+    for (const itemData of todoList) {
+      const itemHtmlRef = createToDoItem(
+        itemData.item,
+        itemData.checked,
+        todoList
+      );
+      itemListContainer.appendChild(itemHtmlRef);
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
+import { createToDoItemList } from "./components/item";
+import { getToDoData, updateUserData, createUserData } from "./utils/api";
+
 console.log("To Do App");
+
+const inputTask = document.getElementById("task-name");
+
+let todo = [];
+let userExist = false;
+
+getToDoData((data) => {
+  console.log(data);
+
+  if (data.id === "zsdemeter") {
+    todo = data.todo;
+    userExist = true;
+    createToDoItemList(todo);
+  }
+});
+
+document.getElementById("add-task-button").addEventListener("click", () => {
+  if (userExist) {
+    // update user data
+    console.log("add task to todo list");
+    const itemValue = inputTask.value;
+    if (itemValue) {
+      console.log(todo);
+
+      todo.push({
+        checked: false,
+        item: itemValue,
+      });
+
+      updateUserData(todo, () => {
+        getToDoData((data) => {
+            console.log(data);
+        
+              todo = data.todo;
+
+              createToDoItemList(todo);
+            
+          });
+      });
+    }
+  } else {
+    // add user to server
+    const itemValue = inputTask.value;
+    if (itemValue) {
+      createUserData(itemValue, () => {
+        getToDoData((data) => {
+            console.log(data);
+              todo = data.todo;
+              createToDoItemList(todo);
+            
+          });
+      });
+    }
+  }
+
+  inputTask.value = "";
+});
