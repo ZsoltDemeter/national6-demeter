082bd10d9e1c79d16ef9bf443bcd28598c0cca1f
ZsoltDemeter <zsoltd.com@gmail.com>
2021-04-15 21:03:40 +0300

    finished course

---
+++ src/components/AppLogo/AppLogo.jsx
@@ -0,0 +1,6 @@
+import todoSvg from "./iron-man.svg";
+import "./AppLogo.css";
+
+export function AppLogo() {
+  return <img src={todoSvg} alt="app-logo" className="app-logo" />;
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { AppLogo } from "../AppLogo/AppLogo";
+import "./Header.css";
+
+export function Header() {
+  return (
+    <div className="app-header">
+      <AppLogo />
+    </div>
+  );
+}
