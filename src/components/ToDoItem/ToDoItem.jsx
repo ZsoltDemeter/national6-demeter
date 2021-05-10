e050d1dd37deb38b3156825b107ad76012625ab4
ZsoltDemeter <zsoltd.com@gmail.com>
2021-05-10 21:39:30 +0300

    remove item icon added

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -12,7 +12,7 @@ export function ToDoItem(props) {
       />
       <p>{props.label}</p>
       <img
-        src="https://www.flaticon.com/svg/vstatic/svg/892/892930.svg?token=exp=1618938206~hmac=b7491265fe8f1f011e746d785487c818"
+        src="/delete.png"
         alt="trash"
         onClick={() => props.removeItem(props.label)}
       />
